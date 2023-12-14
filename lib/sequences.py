22b5d099628d057a9917d8e14d68f074b2f5611b
mrrsmburu <mrrsmburu@gmail.com>
2023-12-14 13:29:17 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fib_list = [0, 1]  
+    if length == 1:
+        print(fib_list[0])
+    elif length == 2:
+        print(fib_list[0], fib_list[1])
+    else:
+        print(fib_list[0], fib_list[1], end=" ")
+        for i in range(2, length):
+            next_num = fib_list[i - 1] + fib_list[i - 2]
+            fib_list.append(next_num)
+            print(next_num, end=" ")
+    print()
     pass
